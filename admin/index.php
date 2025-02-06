c8a7090c525e53b5e3756a8b585897fb455652b6
Tougay <hassanahmat41@gmail.com>
2025-02-06 09:07:52 +0100

    changement du nom du projet

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   	<div class="login-logo">
-  		<b>Système de bibliothèque</b>
+  		<b>Bibliothèque Numérique</b>
   	</div>
   
   	<div class="login-box-body">
